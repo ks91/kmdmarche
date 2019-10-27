d6571a52025bd2f3de80ddebd011b24684a46631
nagomumorita <56901060+nagomumorita@users.noreply.github.com>
2019-10-27 22:20:41 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ title_name="ここたのマルシェ"
 @app.route("/")
 def login():
     now = datetime.datetime.now()
-    timeString = now.strftime("%Y-%m-%d %H:%M")
-
+    # timeString = now.strftime("%Y-%m-%d %H:%M")
+    timeString = now.strftime("%H:%M")
 
     return render_template("login.html", title_name = title_name, time=timeString)
 
@@ -34,8 +34,8 @@ def pay0():
 # 支払い画面
 @app.route("/pay", methods=['POST','GET'])
 def pay():
-    menu_name = "支払うためのQRコードを発行します"
-    info = "下記のQRコードをお客さんに見せてください"
+    menu_name = "Cocotano Marche"
+    info = ""
     code_input = request.form['code']
     # code_input = "hogehogehoge"
     qr_b64data = qrmaker(str(code_input))
@@ -44,7 +44,9 @@ def pay():
     return render_template("pay2.html",
         data=code_input,
         qr_b64data=qr_b64data,
-        qr_name=qr_name
+        qr_name=qr_name,
+        menu_name=menu_name,
+        info=info
     )
     # code_input = request.form['code']
     # fig_url = QRmaker(code_input)
@@ -95,45 +97,58 @@ def qrmaker(code):
 #     return send_file(qrcode(data, mode="raw"), mimetype="image/png")
 @app.route("/sokin")
 def sokin():
-    menu_name = "『まなぶさん』に送金しますか？"
+    who = "BOB"
     info = "送金額を入力してください"
 
-    return render_template("sokin.html", menu_name = menu_name, title_name = title_name, info = info)
+    return render_template("sokin.html", who = who, title_name = title_name, info = info)
 
 @app.route("/sokin_later")
 def sokin_later():
-    menu_name = "『まなぶさん』に送金しました"
-    info = "引き続きお楽しみください！"
 
-    return render_template("sokin_later.html", menu_name = menu_name, title_name = title_name, info = info)
+    return render_template("sokin_later.html")
 
 
 # 出店者登録
 @app.route("/shopowner")
 def shopowner():
-    menu_name = "出店者登録をします"
-    info = "出店者登録は簡単！"
-    return render_template("shopowner.html", menu_name = menu_name, title_name = title_name, info = info)
+    menu_name = "Register a shop!"
+    info = ""
+    now = datetime.datetime.now()
+    timeString = now.strftime("%H:%M")
+    return render_template("shopowner.html", menu_name = menu_name, title_name = title_name, info = info, time=timeString)
+
+@app.route("/shopowener_later")
+def shopowner_later():
+
+    return render_template("shopowner_later.html")
 
 # TX一覧
 @app.route("/tx")
 def tx():
-    menu_name = "TX一覧を表示します"
-    info = "あなたの交換は、下記をチェック"
-    return render_template("tx.html", menu_name = menu_name, title_name = title_name, info = info)
+    menu_name = "Transactions"
+    info = "Your transaction will be listed bellow"
+    now = datetime.datetime.now()
+    timeString = now.strftime("%H:%M")
+    return render_template("tx.html", menu_name = menu_name, title_name = title_name, info = info, time=timeString)
 
 # 出店者一覧
 @app.route("/ownerlist")
 def ownerlist():
-    menu_name = "今、おすすめの出店者はこちら"
-    info = "更新順に表示しています"
-    return render_template("ownerlist.html", menu_name = menu_name, title_name = title_name, info = info)
+    menu_name = "Recommend Shop"
+    info = "Show in Time Order"
+    now = datetime.datetime.now()
+    timeString = now.strftime("%H:%M")
+    return render_template("ownerlist.html", menu_name = menu_name, title_name = title_name, info = info, time=timeString)
 
 @app.route("/mypage")
 def mypage():
-    menu_name = "マイページです"
-    info = "あなたの今のコインは24コイン"
-    return render_template("mypage.html", menu_name = menu_name, title_name = title_name, info = info)
+    menu_name = "My Page"
+    info = "24"
+    spendcoin = "56"
+    getcoin = "9"
+    now = datetime.datetime.now()
+    timeString = now.strftime("%H:%M")
+    return render_template("mypage.html", menu_name = menu_name, title_name = title_name, info = info, time=timeString, spendcoin = spendcoin, getcoin = getcoin)
 
 
 
